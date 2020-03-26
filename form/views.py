35895543c3164a6bca6c9c0a3cadbf963a6cd66c
Nainiket <nainiketdesai98@gmail.com>
2020-03-26 17:43:52 -0400

    one step closer

--- form/views.py
+++ form/views.py
@@ -1,4 +1,6 @@
-import csv
+import csv, requests
+import schedule 
+import time 
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
@@ -46,10 +48,14 @@ def download_csv(request):
     for obj in items:
         writer.writerow([obj.Name, obj.question1, obj.question2, obj.question3, obj.question4, obj.date])
 
-   # print(response.body())
     return response
 
 
 def email(request):
-    send_mail('header', 'hello','todaycovid@gmail.com',['mpatel@dsstoronto.com'], fail_silently = False)
+    r = requests.get("http://127.0.0.1:8000/download-csv")
+    attachment = r.content
+    print(r)
+    send_mail('"Covid_Survey_report_on_' + str(datetime.now())  + '.csv"', "Please find the report in the attachment below",'todaycovid@gmail.com',['mpatel@dsstoronto.com'], fail_silently = False)
     return render(request, 'success.html')
+
+schedule.every().day.at("00:00").do(email) 
